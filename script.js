e9180f10ff5447b9b0100e833c5ca54c2426dffd
Алексей Павлов <brian.iproject@gmail.com>
2019-08-16 14:01:31 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,9 @@
+var ajax = new XMLHttpRequest();
+ajax.open("GET", "images/icons.svg", true);
+ajax.send();
+ajax.onload = function(e) {
+    var svgDiv = document.createElement("div");
+    svgDiv.style.display = "none";
+    svgDiv.innerHTML = ajax.responseText;
+    document.body.insertBefore(svgDiv, document.body.childNodes[0]);
+};
